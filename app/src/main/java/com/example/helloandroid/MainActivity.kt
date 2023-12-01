97d9bb34cef391a8cf03462e6b18dc3346d8837e
kumo <monocloud0426@gmail.com>
2023-12-01 19:43:48 +0900

    tutorial　completion

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,18 +27,31 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Greeting関数テスト（name変数呼び出し）")
+                    Test("〇")
                 }
             }
         }
     }
 }
+@Composable
+fun Test(candy: String){
+Surface(color = Color.LightGray) {
+Text(text = "テストコード$candy"+"チュートリアル6",modifier = Modifier.padding(24.dp))
+}    
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box{
     Text(
             text = "Hello World \n  $name",
-            modifier = modifier.background(Color(0xFFBCE2BD)).height(5.dp)
+            modifier = modifier
+                .background(Color(0xFFBCE2BD))
+                .height(50.dp)
+                .width(1000.dp)
     )
+    }
 }
 
 @Preview(showBackground = true)
